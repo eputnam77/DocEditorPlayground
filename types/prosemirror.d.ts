59a2da7cc9c19c8cf718978c850ab7491b2391f1
Eric Putnam <96210513+eputnam77@users.noreply.github.com>
2025-09-27 16:21:16 -0400

    fix(types): refine prosemirror plugin typings

--- types/prosemirror.d.ts
+++ types/prosemirror.d.ts
@@ -1,17 +1,52 @@
 declare module "prosemirror-state" {
-  export class Plugin {
-    constructor(config: any);
-    getState(state: any): any;
-    props: {
-      decorations(this: { getState(state: any): any }, state: any): any;
-      [key: string]: any;
-    };
-    spec: any;
+  export interface EditorState {
+    doc: unknown;
   }
-  export class PluginKey {
+
+  export interface Transaction {
+    docChanged: boolean;
+    doc: unknown;
+  }
+
+  export interface PluginStateField<State> {
+    init(config: { state: EditorState }, instance: EditorState): State;
+    apply(
+      tr: Transaction,
+      value: State,
+      oldState?: EditorState,
+      newState?: EditorState,
+    ): State;
+  }
+
+  export type DecorationsHandler<State> =
+    | ((this: Plugin<State>, state: EditorState) => import("prosemirror-view").DecorationSet | null | undefined)
+    | ((state: EditorState) => import("prosemirror-view").DecorationSet | null | undefined);
+
+  export interface PluginProps<State> {
+    decorations?: DecorationsHandler<State>;
+    [key: string]: any;
+  }
+
+  export interface PluginSpec<State = unknown> {
+    key?: PluginKey<State>;
+    state?: PluginStateField<State>;
+    props?: PluginProps<State>;
+    [key: string]: any;
+  }
+
+  export class Plugin<State = unknown> {
+    constructor(spec: PluginSpec<State>);
+    readonly spec: PluginSpec<State>;
+    readonly props?: PluginProps<State>;
+    getState(state: EditorState): State | undefined;
+  }
+
+  export class PluginKey<State = unknown> {
     constructor(name?: string);
-    get(state: any): any;
+    get(state: EditorState): Plugin<State> | undefined;
+    getState(state: EditorState): State | undefined;
   }
+
   export const TextSelection: any;
 }
 
@@ -20,5 +55,21 @@ declare module "prosemirror-model" {
     type: { name: string };
     attrs: any;
     text?: string;
+  }
+}
+
+declare module "prosemirror-view" {
+  export class Decoration {
+    static inline(
+      from: number,
+      to: number,
+      attrs?: Record<string, any>,
+      spec?: any,
+    ): Decoration;
+  }
+
+  export class DecorationSet {
+    static empty: DecorationSet;
+    static create(doc: unknown, decorations: Decoration[]): DecorationSet;
   }
 }
